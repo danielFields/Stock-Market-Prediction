43f6edb0d3daa27ef386333e4754317818218c1c
Daniel  Fields <dfieldstherunner@gmail.com>
2019-04-09 18:41:32 -0700

    Fit the model and add Tensorboard

---
+++ LSTM_fit_diagnostics.py
@@ -0,0 +1,16 @@
+#if not already installed, run below
+  # if inside a jupyter notebook as I was: !pip install -q tf-nightly-2.0-preview
+  # command line: pip install -q tf-nightly-2.0-preview
+
+
+# Load the TensorBoard notebook extension
+%load_ext tensorboard.notebook
+
+logdir = os.path.join("logs", dt.datetime.now().strftime("%Y%m%d-%H%M%S"))
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+
+#Call tensorboard to watch model train and understand its architecture better
+%tensorboard --logdir logs
+
+#Train the model
+model.fit(X_trn,y_trn, epochs=1000,batch_size = 4153,validation_split = 0.2, callbacks=[tensorboard_callback], verbose = 0)
