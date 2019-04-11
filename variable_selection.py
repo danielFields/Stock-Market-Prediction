f618e9852b14b6b433e4885972831a9d27103cc6
Daniel  Fields <dfieldstherunner@gmail.com>
2019-04-10 19:48:22 -0700

    Preprocessing Steps

--- variable_selection.py
+++ variable_selection.py
@@ -12,26 +12,21 @@ within_tolerance = num_NA.query("Proportion_NA < 2 & Proportion_NA > 0")
 
 #replace missing values with the mean of each variable of which is within tolerance
 
-data1["DVY"][data1["DVY"].isnull()] = np.mean(data1["DVY"]) #Replace missing values of DVY with mean of DVY
-
-data1["CATY"][data1["CATY"].isnull()] = np.mean(data1["CATY"]) #Replace missing values of CATY with mean of CATY
-
-data1["RELINF"][data1["RELINF"].isnull()] = np.mean(data1["RELINF"]) #Replace missing of RELINF values with mean of RELINF
+#Replace missing values with mean and store in new dataset
+data2 = data1.replace({'DVY': {np.nan:np.mean(data1["DVY"])}, 'CATY': {np.nan:np.mean(data1["CATY"])},'RELINF': {np.nan:np.mean(data1["RELINF"])}})
 
 #Verify that variables selected are all now complete
 num_NA2 = pd.DataFrame()
-num_NA2["Num_NA"] = data1.isnull().sum(axis = 0)
+num_NA2["Num_NA"] = data2.isnull().sum(axis = 0)
 num_NA2["Proportion_NA"] = np.round((num_NA2["Num_NA"]*100)/len(data1["CLOSE"]),3)
 num_NA2.query("Proportion_NA < 2").sort_values("Proportion_NA", ascending = False)
 
-#Variable Selection
-data2 = data1[num_NA2.query("Proportion_NA < 2").index].drop("Date", axis = 1) #Get variables we will be using using 2% NA tolerance
 
 #Examine design matrix for modeling
 data2.info()
 
 #Split data into design matrix and target
-X = data2.drop(["CLOSE"], axis = 1)
+X = data2.drop(["CLOSE","Date"], axis = 1)
 y = data2["CLOSE"]
 
 #Split design matrix and target into training and testing splits and reshape for modeling
